84548e8dce7ffc2f3f0c1e9e666d4fb7c24a2539
james-francis-mt <james.francis.jef@gmail.com>
2025-10-17 23:58:27 +0100

    add setup

--- src/test_framework.py
+++ src/test_framework.py
@@ -1,20 +1,30 @@
-from typing import Callable, cast
+from typing import Callable, cast, override
 
 class TestCase:
     def __init__(self, name: str):
         self.name: str = name
         
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = cast(Callable[[], None], getattr(self, self.name))
         method()
 
+
 class WasRun(TestCase):
     def __init__(self, name: str):
+        self.wasSetUp: int | None = None
         self.wasRun: int | None = None
         TestCase.__init__(self, name)
 
     def testMethod(self):
         self.wasRun = 1
+
+    @override
+    def setUp(self):
+        self.wasSetUp = 1
 
 class TestCaseTest(TestCase):
     def testRunning(self):
@@ -22,4 +32,13 @@ class TestCaseTest(TestCase):
         assert(not test.wasRun)
         test.run()
         assert(test.wasRun)
+
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)
+
+print('running tests')
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
+print('tests okay')
