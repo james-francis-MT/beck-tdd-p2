96d5ff613899cf7f5c77af103cfbac2b1e152fe3
james-francis-mt <james.francis.jef@gmail.com>
2025-10-17 23:43:48 +0100

    idiomatic python

--- src/test_framework.py
+++ src/test_framework.py
@@ -1,7 +1,12 @@
+from typing import Callable, cast
+
+
 class WasRun:
     def __init__(self, name: str):
         self.wasRun: int | None = None
+        self.name: str = name
     def run(self):
-        self.testMethod()
+        method = cast(Callable[[], None], getattr(self, self.name))
+        method()
     def testMethod(self):
         self.wasRun = 1
--- tests/test_test_framework.py
+++ tests/test_test_framework.py
@@ -1,7 +1,8 @@
 from src.test_framework import WasRun
 
 def test_test_framework():
-    test = WasRun("testMehod")
+    test = WasRun("testMethod")
+    print(type(test.run))
     assert(test.wasRun == None)
     test.run()
     assert(test.wasRun == 1)
